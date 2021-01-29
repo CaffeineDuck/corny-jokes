57675fdcde2565a5f3ba98799b2cbbb660960c55
Samrid Pandit <samrid@pop-os.localdomain>
2021-01-29 21:19:35 +0545

    Boilerplate

---
+++ pages/about.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		
+  	<h1>About Dad Jokes</h1>
+
+  	<p>
+      This is an app that displays corny dad jokes.
+  	</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
