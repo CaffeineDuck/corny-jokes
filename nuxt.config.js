d3a7a70ba77a04e2700dfaa54bda65f1405ff814
Samrid Pandit <samrid@pop-os.localdomain>
2021-01-30 21:23:35 +0545

    Fixed the description in meta tags of main site

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'Best place for Corny Jokes!' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.svg' }
